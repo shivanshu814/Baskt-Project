eec57c68a3fcc94859575a47089c284f7a4e81b0
Shivanshu Pathak <shivanshupathak814@gmail.com>
2025-06-02 23:49:09 -0700

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,68 +1,67 @@
 module.exports = {
-    apps: [
-      {
-        name: "oracle:manager",
-        cwd: "/root/monorepo/services/oracle",
-        script: "bash",
-        args: "-c 'npx tsx src/manager.ts'",
-        interpreter: "none",
-        exec_mode: "fork",
-        autorestart: true,
-        watch: false,
-        env: {
-          NODE_ENV: "production"
-        }
+  apps: [
+    {
+      name: 'oracle:manager',
+      cwd: '/root/monorepo/services/oracle',
+      script: 'bash',
+      args: "-c 'npx tsx src/manager.ts'",
+      interpreter: 'none',
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
       },
-      {
-        name: "oracle:worker",
-        cwd: "/root/monorepo/services/oracle",
-        script: "bash",
-        args: "-c 'npx tsx src/worker.ts'",
-        interpreter: "none",
-        exec_mode: "fork",
-        autorestart: true,
-        watch: false,
-        env: {
-          NODE_ENV: "production"
-        }
+    },
+    {
+      name: 'oracle:worker',
+      cwd: '/root/monorepo/services/oracle',
+      script: 'bash',
+      args: "-c 'npx tsx src/oracle-worker.ts'",
+      interpreter: 'none',
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
       },
-      {
-        name: "backend",
-        cwd: "/root/monorepo/services/backend",
-        script: "bash",
-        args: "-c 'npx tsx src/server/index.ts'",
-        interpreter: "none",
-        exec_mode: "fork",
-        autorestart: true,
-        watch: false,
-        env: {
-          NODE_ENV: "production"
-        }
-      }, 
-      {
-        name: "event-listener",
-        cwd: "/root/monorepo/services/event-engine",
-        script: "bash",
-        args: "-c 'npx tsx src/listener.ts'",
-        interpreter: "none",
-        exec_mode: "fork",
-        autorestart: true,
-        watch: false,
-        env: {
-          NODE_ENV: "production"
-        }
+    },
+    {
+      name: 'backend',
+      cwd: '/root/monorepo/services/backend',
+      script: 'bash',
+      args: "-c 'npx tsx src/server/index.ts'",
+      interpreter: 'none',
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
       },
-      {
-        name: "event-worker",
-        cwd: "/root/monorepo/services/event-engine",
-        script: "bash",
-        args: "-c 'npx tsx src/worker.ts'",
-        autorestart: true, 
-        watch: false,
-        env: {
-          NODE_ENV: "production"
-        }
-      }
-    ]
-  }
-  
+    },
+    {
+      name: 'event-listener',
+      cwd: '/root/monorepo/services/event-engine',
+      script: 'bash',
+      args: "-c 'npx tsx src/listener.ts'",
+      interpreter: 'none',
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'event-worker',
+      cwd: '/root/monorepo/services/event-engine',
+      script: 'bash',
+      args: "-c 'npx tsx src/worker.ts'",
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
