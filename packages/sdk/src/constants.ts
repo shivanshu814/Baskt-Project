00bc3e90d689c001439747786023a22d1b89835d
Shivanshu Pathak <shivanshupathak814@gmail.com>
2025-06-29 12:32:17 -0400

    more details are allow

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,6 +1,6 @@
 import { PublicKey } from '@solana/web3.js';
 
 export const MAX_PRICE_AGE_SEC = 60;
-export const MAX_ASSET_PRICE_AGE_MS = 60 * 1000;
+export const MAX_ASSET_PRICE_AGE_MS = 90 * 1000;
 
 export const USDC_MINT = new PublicKey(process.env.NEXT_PUBLIC_USDC_MINT || 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
