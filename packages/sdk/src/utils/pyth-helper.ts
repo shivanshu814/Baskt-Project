64081a3833d9aa021de7b73f765ed1314d8f2569
Shivanshu Pathak <shivanshupathak814@gmail.com>
2025-07-08 11:00:47 -0400

    GG (#166)
    * feat(fixes): ui prod fixes (#164)
    
    * feat(fixes): ui prod fixes
    
    * feat(baskt-details): added ui changes to baskt -details in every tab
    
    * feat(orders): listing fast
    
    * move to priority fee
    
    ---------
    
    Co-authored-by: Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>

--- packages/sdk/src/utils/pyth-helper.ts
+++
@@ -1,4 +0,0 @@
-// import {  } from '@pythnetwork/pyth-solana-receiver';
-// export function getPythPriceAccount(feedId: string) {
-//   return getPriceFeedAccountForProgram(0, feedId).toBase58();
-// }
